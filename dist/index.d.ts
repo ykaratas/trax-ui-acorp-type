2c88eea25c6baa394473e7011ad5b202b30a577e
yucelkaratas <yucelkaratas@gmail.com>
2026-02-10 02:00:48 +0200

    aaa

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,11 @@
 export type TxType = 'DEPOSIT' | 'WITHDRAWAL' | 'TRANSFER' | 'UNSET';
 
 export interface Tx {
-    id?: number | null;
+    id?: string | null;
     clientSystemId: string;
     txType: TxType;
     customerId: string;
+    accountId?: string | null;
     txDateTime?: number;
     data: Record<string, unknown>;
 }
